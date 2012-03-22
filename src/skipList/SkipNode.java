8c2e9516dccad417c2e55cb4a11341f07bc6a739
Wyatt <wpillmore@gmail.com>
2012-03-22 02:25:34 -0600

    Updated with comments and new Node class. Changes include height and null set on next constructor.

--- src/skipList/SkipNode.java
+++ src/skipList/SkipNode.java
@@ -2,21 +2,25 @@ package skipList;
 
 /**
  * @author Wyatt Pillmore
+ * 
  * skipNode Class constructor consisting of skipNode array and public data.
  * Constructor sets height of array and value of data.
  * Implements Comparable as requested.
+ * 
  */
 public class SkipNode<T extends Comparable<? super T>> {
 
-	public final SkipNode<T>[] nodeArray;
-	public final T data;
+	public SkipNode<T>[] next;
+	public T data;
+	public int height;
 	
 	@SuppressWarnings("unchecked")
-	public SkipNode(T data, int height) {
-		nodeArray = new SkipNode[height];
+	public SkipNode (T data, int level) {
+		next = new SkipNode[level];
+		height = level;
 		this.data = data;
-	}
-	public int height() {
-		return nodeArray.length - 1;
+		
+		for(int i = 0; i < next.length; i++)
+			next[i] = null;
 	}
 }
