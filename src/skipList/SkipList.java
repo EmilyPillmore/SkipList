ab2cc3564013ab04ddf829b75997eeb3f7678178
Wyatt <wpillmore@gmail.com>
2012-03-22 19:28:55 -0600

    Updated various lexical knickknacks.

--- src/skipList/SkipList.java
+++ src/skipList/SkipList.java
@@ -13,9 +13,9 @@ import java.util.Random;
 
 public class SkipList<T extends Comparable<? super T>> {
    
-	public SkipNode<T> head; 
+	private  SkipNode<T> root; 
 	protected SkipNode<T> cPointer; //current reference pointer node.
-	protected SkipNode<T>[] update; //updateable pointer array
+	protected SkipNode<T>[] pArray; //pArrayable pointer array
 	public final int maxLevel;
 	int size = 0, level;
 	
@@ -25,9 +25,9 @@ public class SkipList<T extends Comparable<? super T>> {
 	 */
 	public SkipList(int maxLevel) {
 		this.maxLevel = maxLevel;
-		head = new SkipNode<T>(null, maxLevel); 
+		root = new SkipNode<T>(null, maxLevel); 
 		for(int i = 0; i < maxLevel; i++)
-			head.next[i] = null;
+			root.next[i] = null;
 	}
 	
 	/**
@@ -41,31 +41,31 @@ public class SkipList<T extends Comparable<? super T>> {
 	}
 	
 	/**
-	 * Method updates using update(value) and inserts node appropriately
+	 * Method pArrays using pArray(value) and inserts node appropriately
 	 * @param value - value to be inserted
 	 */
 	public void insert(T value) {
 		if(value == null)
 			return;
-		else update(value);
+		else updatePointers(value);
 		
-		SkipNode<T> node = cPointer;
-		SkipNode<T>[] temp = update;
+		SkipNode<T> node_t = cPointer;
+		SkipNode<T>[] array_t = pArray;
 		
-		if(node == null || ! node.data.equals(value)) {
-			int lvl = generateRandomLevel();
+		if(node_t == null || ! node_t.data.equals(value)) {
+			int level_t = generateRandomLevel();
 			
-			if(lvl > level) {
-				for(int i = level + 1; i <= lvl; i++) {
-					temp[i] = head;
+			if(level_t > level) {
+				for(int i = level + 1; i <= level_t; i++) {
+					array_t[i] = root;
 				}
-				level = lvl;
+				level = level_t;
 			}
 			
-			node = new SkipNode<T>(value, lvl);
-			for(int i = 0; i <= lvl; i++) {
-				node.next[i] = temp[i].next[i];
-				temp[i].next[i] = node;
+			node_t = new SkipNode<T>(value, level_t);
+			for(int i = 0; i <= level_t; i++) {
+				node_t.next[i] = array_t[i].next[i];
+				array_t[i].next[i] = node_t;
 			}
 		}
 		size++;
@@ -77,36 +77,36 @@ public class SkipList<T extends Comparable<? super T>> {
 	 * @param values - collection of values to be inserted.
 	 */
 	protected void insertAll(Collection<T> values) {
-		for(T x : values) 
-			if (x == null) 
+		for(T type_t : values) 
+			if (type_t == null) 
 				return;
 			else {
-				update(x);
-				insert(x);
+				updatePointers(type_t);
+				insert(type_t);
 				size++;
 			}
 	}
 	
 	/**
-	 * Updates and removes references to node with designated value and
+	 * pArrays and removes references to node with designated value and
 	 * decrements level as needed.
 	 * @param value - Value to be deleted.
 	 */
 	public void delete(T value) {
 		if(value == null || isEmpty() == true)
 			return;
-		else update(value);
+		else updatePointers(value);
 	
-		SkipNode<T> node = cPointer;
-		SkipNode<T>[] temp = update;
-		if(node.data.equals(value)) {
+		SkipNode<T> node_t = cPointer;
+		SkipNode<T>[] array_t = pArray;
+		if(node_t.data.equals(value)) {
 			for(int i = 0; i <= level; i++) {
-				if(temp[i].next[i] != node)
+				if(array_t[i].next[i] != node_t)
 					break;
-				temp[i].next[i] = node.next[i];
+				array_t[i].next[i] = node_t.next[i];
 			}
 			
-			while(level > 0 && head.next[level] == null)
+			while(level > 0 && root.next[level] == null)
 				level--;
 		}
 		size--;
@@ -117,12 +117,12 @@ public class SkipList<T extends Comparable<? super T>> {
 	 * @param values - collection to be deleted.
 	 */
 	protected void deleteAll(Collection<T> values) {
-		for(T x : values) 
-			if (x == null) 
+		for(T type_t : values) 
+			if (type_t == null) 
 				return;
 			else {
-				update(x);
-				delete(x);
+				updatePointers(type_t);
+				delete(type_t);
 				size--;
 			}
 	}
@@ -135,11 +135,11 @@ public class SkipList<T extends Comparable<? super T>> {
 	public SkipNode<T> find(T value) {
 		if(value == null || isEmpty() == true)
 			return null;
-		else update(value);
+		else updatePointers(value);
 
-		SkipNode<T> x = cPointer;
-		return x != null && x.data.equals(value) 
-				? x : null;
+		SkipNode<T> node_t = cPointer;
+		return node_t != null && node_t.data.equals(value) 
+				? node_t : null;
 	}
 	
 	/**
@@ -151,33 +151,33 @@ public class SkipList<T extends Comparable<? super T>> {
 	public boolean contains(T value) {
 		if(value == null || isEmpty() == true)
 			return false;
-		else update(value);
+		else updatePointers(value);
 		
-		SkipNode<T> x = cPointer;
-		return x != null && x.data.equals(value);
+		SkipNode<T> node_t = cPointer;
+		return node_t != null && node_t.data.equals(value);
 	}
 	
 	/**
-	 * Updater method to update our current Pointer (cPointer)
+	 * pArrayr method to pArray our current Pointer (cPointer)
 	 * and references.
 	 * @param value - value to set cPointer reference to.
 	 */
 	@SuppressWarnings("unchecked")
-	protected void update(T value) {
-		if(head == null)
-			head = new SkipNode<T>(null, maxLevel);
+	protected void updatePointers(T value) {
+		if(root == null)
+			root = new SkipNode<T>(null, maxLevel);
 		
-		SkipNode<T> x = head;
-		SkipNode<T> [] temp = new SkipNode[maxLevel + 1];
+		SkipNode<T> node_t = root;
+		SkipNode<T> [] array_t = new SkipNode[maxLevel + 1];
 		
 		for(int i = level; i >= 0; i--) {
-			while(x.next[i] != null && x.next[i].data.compareTo(value) < 0) {
-				x = x.next[i];
+			while(node_t.next[i] != null && node_t.next[i].data.compareTo(value) < 0) {
+				node_t = node_t.next[i];
 			}
-			temp[i] = x;
+			array_t[i] = node_t;
 		}
-		cPointer = x.next[0];
-		update = temp;
+		cPointer = node_t.next[0];
+		pArray = array_t;
 	}
 
 /* ----------- Utility Methods ------------ */
@@ -192,17 +192,17 @@ public class SkipList<T extends Comparable<? super T>> {
 	
 	/**
 	 * Method to safeguard against null skipList.
-	 * @return true if head == null, or false if not;
+	 * @return true if root == null, or false if not;
 	 */
 	protected boolean isEmpty() {
-		return head == null ? true : false;
+		return root == null ? true : false;
 	}
 	
 	/**
-	 * @return an empty head, creating a null SkipList.
+	 * @return an empty root, creating a null SkipList.
 	 */
 	public void makeEmpty() {
-		head = null;
+		root = null;
 		size = 0;
 	}
 	
